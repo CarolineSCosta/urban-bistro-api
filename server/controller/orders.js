9f114a544402388dd63ee6497d3849a2ee26a93e
caroline costa <carolmarcionilia@gmail.com>
2021-03-31 21:03:26 -0300

    add findByPk order

--- server/controller/orders.js
+++ server/controller/orders.js
@@ -63,6 +63,43 @@ class OrdersController {
       return res.status(400).json({ message: error.message });
     }
   }
+
+  static async getOrderById(req, res) {
+    try {
+      const order = await database.Orders.findByPk(req.params.id, {
+        include: [{
+          model: database.Products,
+          as: 'products',
+          attributes: ['id', 'name', 'flavor', 'complement'],
+        }],
+        through: {
+          model: database.ProductOrders,
+          attributes: ['qtd'],
+        },
+      });
+
+      return res.status(200).json(
+        {
+          id: order.id,
+          client_name: order.client_name,
+          user_id: order.user_id,
+          table: order.table,
+          status: order.status,
+          createdAt: order.createdAt,
+          updatedAt: order.updatedAt,
+          products: order.products.map((product) => ({
+            id: product.id,
+            name: product.name,
+            flavor: product.flavor,
+            complement: product.complement,
+            qtd: product.ProductOrders.qtd,
+          })),
+        },
+      );
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
 }
 
 module.exports = OrdersController;
